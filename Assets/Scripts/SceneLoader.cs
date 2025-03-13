42ea74ba83d30067c08dc34e6db89af8479fce22
Bram <bram.salomons@gmail.com>
2025-03-13 22:47:29 +0100

    Added pretty looking start screen

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField]
     private string sceneName;
 
+    [SerializeField]
+    GameObject LoadingScreen;
+
+    [SerializeField]
+    Slider loadBar;
 
     public void LoadScene()
     {
@@ -16,5 +22,32 @@ public class SceneLoader : MonoBehaviour
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadSceneScreen()
+    {
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    IEnumerator LoadSceneAsync(string name)
+    {
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / .9f);
+
+            loadBar.value = progressValue;
+            
+            yield return null;
+        }
+
     }
 }
